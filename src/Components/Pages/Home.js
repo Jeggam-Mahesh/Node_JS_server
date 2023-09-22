a9a1c6f8bd6e76b2f80ce2e5b39c63ef054615ef
Jeggam-Mahesh <jeggammahesh15720@gmail.com>
2023-09-22 22:37:53 +0530

    jj

--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -72,7 +72,7 @@ const Home=()=>{
             </div>
             <div className="post_container">
                     <h1 className="topic_heading">Top Posts</h1>
-                    <div className="advertisement">
+                    <div className="homeAdvertisement">
                         
                         <img className='Adds' src='https://i.pinimg.com/originals/27/94/6a/27946a99657cddf0cbde79a7e4e6f51f.gif' alt="not found" width='400px' height='600px'/>
                         </div>
